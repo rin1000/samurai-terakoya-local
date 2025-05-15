18afec09e1a6ed66ace1d018462502c9dea53c56
rin1000 <rin.ichikawa@d2c.co.jp>
2025-05-15 17:20:19 +0900

    Update calc.js

--- JavaScript/kadai_007/calc.js
+++ JavaScript/kadai_007/calc.js
@@ -1,11 +1,11 @@
 let num = 123;
 
 if(num % 15 == 0){
-console("3と5の倍数です");
+console.log("3と5の倍数です");
 }else if(num % 5 == 0){
-console("5の倍数です");
+console.log("5の倍数です");
 }else if(num % 3 == 0){
-console("3の倍数です");
+console.log("3の倍数です");
 }else{
-    console(num);
+    console.log(num);
 };
